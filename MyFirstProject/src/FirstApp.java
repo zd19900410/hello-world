3c257470d00232ba51447c9910df34b51801499a
zd19900410 <zhaodu@gmail.com>
2020-05-12 20:04:17 -0400

    Add files via upload

---
+++ MyFirstProject/src/FirstApp.java
@@ -0,0 +1,12 @@
+/**
+ * filename:FirstApp.java My first JAVA program!
+ * 
+ * @author dzhao
+ *
+ */
+public class FirstApp {
+	public static void main(String[] args) {
+		System.out.println("I love programming!!!");
+	}
+
+}
